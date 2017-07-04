e7f5b2d6297d98084455e509acbbcafd624f0937
george-spitfire <george@spitfireaudio.com>
2017-07-04 12:11:20 +0100

    change comment

--- tutorial_simple_synth_noise/Source/MainComponent.cpp
+++ tutorial_simple_synth_noise/Source/MainComponent.cpp
@@ -20,7 +20,7 @@ public:
 
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
     {
-        //blah
+        //blah2
         String message;
         message << "Preparing to play audio...\n";
         message << " samplesPerBlockExpected = " << samplesPerBlockExpected << "\n";
