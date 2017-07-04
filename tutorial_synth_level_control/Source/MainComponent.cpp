ea0cda560f0cfc126889dd0da34f7882c3f350c2
george-spitfire <george@spitfireaudio.com>
2017-07-04 15:50:25 +0100

    add tutorial 2 files

---
+++ tutorial_synth_level_control/Source/MainComponent.cpp
@@ -0,0 +1,69 @@
+#ifndef MAINCOMPONENT_H_INCLUDED
+#define MAINCOMPONENT_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class MainContentComponent   : public AudioAppComponent
+{
+public:
+    MainContentComponent()
+    {
+        levelSlider.setRange (0.0, 0.25);
+        levelSlider.setTextBoxStyle (Slider::TextBoxRight, false, 100, 20);
+        levelLabel.setText ("Noise Level", dontSendNotification);
+        
+        addAndMakeVisible (levelSlider);
+        addAndMakeVisible (levelLabel);
+        
+        setSize (600, 100);
+        setAudioChannels (0, 2);
+    }
+
+    ~MainContentComponent()
+    {
+        shutdownAudio();
+    }
+
+    void prepareToPlay (int /*samplesPerBlockExpected*/, double /*sampleRate*/) override
+    {
+    }
+
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
+    {
+        const float level = (float) levelSlider.getValue();
+        
+        for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
+        {
+            float* const buffer = bufferToFill.buffer->getWritePointer (channel, bufferToFill.startSample);
+            
+            for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
+            {
+                const float noise = random.nextFloat() * 2.0f - 1.0f;
+                buffer[sample] = noise * level;
+            }
+        }
+    }
+    
+    void releaseResources() override
+    {
+    }
+
+    void resized() override
+    {
+        levelLabel.setBounds (10, 10, 90, 20);
+        levelSlider.setBounds (100, 10, getWidth() - 110, 20);
+    }
+    
+
+private:
+    Random random;
+    Slider levelSlider;
+    Label levelLabel;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
+};
+
+Component* createMainContentComponent()     { return new MainContentComponent(); }
+
+
+#endif  // MAINCOMPONENT_H_INCLUDED
